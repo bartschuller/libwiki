e36be1d031da3b07da597976d6f2f876501db35b
Bart Schuller <schuller@lunatech.com>
2010-08-29 14:27:26 +0200

    more inline, blockquote, ul WIP

--- src/main/scala/org/smop/libwiki/textile/TextileParser.scala
+++ src/main/scala/org/smop/libwiki/textile/TextileParser.scala
@@ -6,18 +6,27 @@ import xml.{Text, TopScope, Elem, NodeSeq, Null}
 class TextileParsers extends RegexParsers {
   override def skipWhitespace = false
   def doc: Parser[NodeSeq] = rep(block) ^^ (_.flatten)
-  def block: Parser[NodeSeq] = heading | para
+  def block: Parser[NodeSeq] = heading | blockquote | para
   def heading: Parser[NodeSeq] = ("^h".r ~> "[1-6]".r <~ """\.\s+""".r) ~ lines ^^
           { case n~content => Elem(null, "h"+n, Null, TopScope, content: _*)}
+  def blockquote: Parser[NodeSeq] = ("""^bq\.\s+""".r) ~> para ^^
+          (x => <blockquote>{x}</blockquote>)
+  def unordered: Parser[NodeSeq] = ("""^\*\s+""".r) ~> listlines ^^
+          (x => <ul>{x}</ul>)
+  def listlines: Parser[NodeSeq] = ((rep1sep(inline, eol) ^^
+          (_.reduceLeft((_ : NodeSeq) ++ <br/> ++ _))) ^^
+          (_.flatten : NodeSeq)) <~ rep(eol)
   def para: Parser[NodeSeq] = lines ^^ (x => <p>{x}</p>)
   def eol: Parser[Any] = """\r?\n""".r
   def lines: Parser[NodeSeq] = ((rep1sep(inline, eol) ^^
           (_.reduceLeft((_ : NodeSeq) ++ <br/> ++ _))) ^^
-          ( x => x.reduceLeft((_: NodeSeq) ++ _))) <~ rep(eol)
-  def inline: Parser[NodeSeq] = italic | emphasis |plaintext //  bold | strong |
-  def plaintext = """\S.*(?<!_)""".r ^^ (Text(_))
+          (_.flatten : NodeSeq)) <~ rep(eol)
+  def inline: Parser[NodeSeq] = italic | emphasis | bold | strong |plaintext
+  def plaintext = """\S.*(?<![_*])""".r ^^ (Text(_))
   def italic = "__" ~> (inline ^^ (x => <i>{x}</i>)) <~ "__"
   def emphasis = "_" ~> (inline ^^ (x => <em>{x}</em>)) <~ "_"
+  def bold = "**" ~> (inline ^^ (x => <b>{x}</b>)) <~ "**"
+  def strong = "*" ~> (inline ^^ (x => <strong>{x}</strong>)) <~ "*"
 }
 
 object TextileParser extends TextileParsers {
--- src/test/scala/textile.scala
+++ src/test/scala/textile.scala
@@ -16,11 +16,20 @@ object TextileSpec extends Specification {
     "a mix of headings and paragraphs" in {
       TextileParser.parseDoc("h1. Hello\n\nthere\n\nh2. h2") must ==/(<h1>Hello</h1><p>there</p><h2>h2</h2>)
     }
-    "italics" in {
+    "blockquotes" in {
+      TextileParser.parseDoc("bq. quoted") must ==/(<blockquote><p>quoted</p></blockquote>)
+    }
+    "italic" in {
       TextileParser.parseDoc("__Hello__") must ==/(<p><i>Hello</i></p>)
     }
     "emphasis" in {
       TextileParser.parseDoc("_Hello_") must ==/(<p><em>Hello</em></p>)
+    }
+    "bold" in {
+      TextileParser.parseDoc("**Hello**") must ==/(<p><b>Hello</b></p>)
+    }
+    "strong" in {
+      TextileParser.parseDoc("*Hello*") must ==/(<p><strong>Hello</strong></p>)
     }
   }
 }

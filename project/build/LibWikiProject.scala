2bc419f4c174a0cf57a9cb3dd6b73e3840d47eb1
Bart Schuller <schuller@lunatech.com>
2010-08-29 22:24:42 +0200

    Now use sbt-idea integration as processor

--- project/build/LibWikiProject.scala
+++ project/build/LibWikiProject.scala
@@ -1,5 +1,5 @@
 import sbt._
 
-class LibWikiProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+class LibWikiProject(info: ProjectInfo) extends DefaultProject(info) {
     val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
 }
